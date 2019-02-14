79507ca7b5ef3ab8b9aac83551ee8cb98837c04d
jaredp1234 <jaredp1234@gmail.com>
2019-02-14 13:15:49 -0800

    one more thing

--- Wheat/main.py
+++ Wheat/main.py
@@ -6,10 +6,10 @@ class Lesson0(Widget): #this defines the instance of the widget.
     pass # pass is used to keep the class valid but allow it not to contain anything - At the moment our widget is not defined.
 
 
-class MyApp(App):
+class Wheat(App):
     def build(self):
         return Lesson0()
 
 if __name__ == '__main__': #Documentation suggests that each program file should be called main.py but I think that only matters if you're creating the final App to go onto a phone or tablet we're a long way off from that yet
 
-    MyApp().run() #This must match the name of your App
+    Wheat().run() #This must match the name of your App
