4920b4470fbfb53b225ddae3b3d77ddf9485164a
Conor Muldoon <conormmuldoon@gmail.com>
2019-04-13 18:03:08 -0700

    hide input button works for latex widget, but does not toggle correctly

--- Wheat/Tex.py
+++ Wheat/Tex.py
@@ -32,7 +32,15 @@ class Display(FloatLayout):
     pass
 
 class Input(FloatLayout):
-    pass
+    def hide_input(wid, dohide=True):
+        if hasattr(wid, 'saved_attrs'):
+            if not dohide:
+                wid.height, wid.size_hint_y, wid.opacity, wid.disabled = wid.saved_attrs
+                del wid.saved_attrs
+
+        elif dohide:
+            wid.saved_attrs = wid.height, wid.size_hint_y, wid.opacity, wid.disabled
+            wid.height, wid.size_hint_y, wid.opacity, wid.disabled = 0, None, 0, True
 
 
 class Tex(ScatterLayout):
